d23946dd3b7b659a228ef4b4babd33c8ca25eab6
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2017-12-07 17:59:58 +0000

    Added intro and data

--- Project/data.js
+++ Project/data.js
@@ -19,5 +19,97 @@ var roomArray = [
               buttonText:'Illuminate'
             }
         ]
+    },
+    {
+        title:'Your room',
+        text:'Your push moves the chest of drawers, catching the detective’s attention, but without further commotion he goes back to searching for clues.',
+        choices:[
+            {
+                text:'You realise you didn’t successfully draw the detective\'s attention to the fork',
+                index:7,
+                buttonText:'Continue'
+            },
+        ]
+    },
+    {
+        title:'Your room',
+        text:'The lock makes a satisfying click as it unlocks.',
+        choices:[
+            {
+                text:'Push the drawer open',
+                index:4,
+                buttonText:'Push'
+            },
+            {
+                text:'Illuminate the drawer',
+                index:5,
+                buttonText:'Illuminate'
+            }
+        ]
+    },
+    {
+        title:'Your room',
+        text:'The detective notices the antique chest of drawers and reminisces about the time he spent round his grandma\'s house, realising he was distracting himself he returns to searching for clues.',
+        choices:[
+            {
+                text:'You realise you didn’t successfully draw the detective\'s attention to the fork',
+                index:7,
+                buttonText:'Continue'
+            }
+        ]
+    },
+    {
+        title:'Your room',
+        text:'The drawer slides open revealing its contents.',
+        choices:[
+            {
+                text:'Illuminate the fork inside the drawer',
+                index:6,
+                buttonText:'Illuminate'
+            }
+        ]
+    },
+    {
+        title:'Your room',
+        text:'He notices the drawer and the unusual engraving on the front, he wonders if this chest was made by Niles Tool Works in America.',
+        choices:[
+            {
+                text:'You realise you didn’t successfully draw the detective\'s attention to the fork',
+                index:7,
+                buttonText:'Continue'
+            }
+        ]
+    },
+    {
+        title:'Your room',
+        text:'He notices the fork inside the drawer and the blood stained onto it. This must be a clue, he thinks to himself, and stashes it into an evidence bag.',
+        choices:[
+            {
+                text:'You successfully drew the detective to the clue',
+                index:7,
+                buttonText:'Continue'
+            }
+        ]
+    },
+    {
+        title:'Kitchen',
+        text:'You realise you can use your powers to guide the detective towards the relevant rooms. The next clue is hidden in the kitchen, the knife used to stab you. The knife is soaking in the water with the other cutlery from preparing the dinner for the guests. The water has become bloody from when the knife was dropped carelessly inside. ',
+        choices:[
+            {
+                text:'Push the knife away from the others in the sink',
+                index:8,
+                buttonText:'Push'
+            },
+            {
+                text:'Possess the plug to drain the sink',
+                index:9,
+                buttonText:'Possess'
+            },
+            {
+                text:'Illuminate the bloody water in the sink',
+                index:10,
+                buttonText:'Illuminate'
+            }
+        ]
     }
 ]
